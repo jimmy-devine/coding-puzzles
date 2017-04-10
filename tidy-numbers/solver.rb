1aaf46b5fb0b2ded5400d9bbc102728afd81cf2d
Jimmy Devine <jimmy@carvinesoftware.com>
2017-04-10 07:17:37 -0400

    Adds tidy numbers

--- tidy-numbers/solver.rb
+++ tidy-numbers/solver.rb
@@ -4,14 +4,39 @@ class Solver
 
     return number if _tidy?(number)
 
-    while !_tidy?(number) do
-      number -= 1
-    end
-    
-    number
+    _make_tidy(number)
   end
 
   private
+  def self._make_tidy(number)
+    numArray = number.to_s.split("").map(&:to_i)
+
+    biggestNumber = numArray.shift
+    output = [biggestNumber]
+    currentNumber = numArray[0]
+
+    while numArray.length > 0
+      if currentNumber < biggestNumber
+        output = self._determine(output, numArray)
+        output[0] = "" if output[0] == "0"
+        return output
+      else
+        biggestNumber = currentNumber
+        output << numArray.shift
+        currentNumber = numArray[0]
+      end
+    end
+  end
+
+  def self._determine(left, right)
+    if left.last == 1
+      return "9" * (left.length + right.length - 1)
+    end
+
+    left_minus_1 = left.join("").to_i - 1
+    return left_minus_1.to_s + ("9" * right.length) if _tidy?(left_minus_1)
+    return (left[0] - 1).to_s + ( "9" * (left.length + right.length - 1))
+  end
 
   def self._tidy?(number)
     numArray = number.to_s.split("").map(&:to_i)
